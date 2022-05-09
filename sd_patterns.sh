2658d79430ddf91e2bf6a6b419f4fe30565536bf
Phillip Benzinger <th3geekguy@gmail.com>
2022-05-09 11:49:15 -0400

    MOD: patterns update

--- sd_patterns.sh
+++ sd_patterns.sh
@@ -7,20 +7,20 @@
 
 search_sd() {
 	PATTERN_PRINT=false
-  for file in $( grep -r -l "$PATTERN" . ) 
-  do
+	for file in $( grep -r -l "$PATTERN" . ) 
+	do
 		if [ "$PATTERN_PRINT" = false ]; then
 			echo -e "\nThe pattern \x1b[31m'$PATTERN'\x1b[0m appears "
 			PATTERN_PRINT=true
 		fi
-    NODE=${file%%/*}
-    NUMBER_OF_OCCURENCES=$(grep "$PATTERN" $file | wc -l )
-    if [ $NUMBER_OF_OCCURENCES -gt 0 ]; then
+		NODE=${file%%/*}
+		NUMBER_OF_OCCURENCES=$(grep "$PATTERN" $file | wc -l )
+		if [ $NUMBER_OF_OCCURENCES -gt 0 ]; then
 			echo -e "\n    \x1b[34m$NUMBER_OF_OCCURENCES times in file\x1b[0m: \x1b[32m$file\x1b[0m and last line:\n"
-      LAST_LINE=$(grep  "$PATTERN" $file | tail -1)
-      echo -e "        $LAST_LINE"
-    fi
-  done
+			LAST_LINE=$(grep  "$PATTERN" $file | tail -1)
+			echo -e "        $LAST_LINE"
+		fi
+	done
 	if $PATTERN_PRINT; then
 		printf "%`tput cols`s"|sed "s/ /─/g"
 	fi
@@ -28,49 +28,68 @@ search_sd() {
 
 #### 'containers with unready status'  ## this produces very long lines, and I am not sure if it is helpful to search for, so I took it out for now
 PATTERNS=(
-'"level":"fatal"'
-'left gossip cluster'
-"healthscore:[2-9] (connectivity issues)"
-'with result "error:context canceled" took too long '
-'has prevented the request from succeeding (get secrets)'
-'is in state down: Unhealthy UCP manager: ERROR: RethinkDB Health check timed out'
-'is in state down: Awaiting healthy status in classic node inventory - current status: Unhealthy'
-'is in state down: heartbeat failure for node in'
-'Error from leadership election follower'
-'Cluster leadership lost'
-"heartbeat to manager .* failed"
-'etcd cluster is unavailable or misconfigured'
-'Failed to create existing container'
-'Failed to load config file'
-'Failed to allocate address: Invalid address space'
-' error="failed to allocate network IP for task '
-'Failed to delegate: Failed to allocate address: No available addresses'
-'Failed to start certificate controller: error reading CA cert file'
-'unable to query [dD][bB]: rethinkdb'
-'unable to create event in database: rethinkdb: Cannot perform write:'
-'unable to create job: unable to insert job into db: rethinkdb: Cannot perform write:'
-'RethinkDB Health check timed out'
-'handshake failed'
-"TLS handshake error from [1][^72]"
-'failed to complete security handshake from'
-'certificate has expired or is not yet valid'
-'"OOMKilled":true'
-'invoked oom-killer'
-'[Cc]onnection refused '
-'rejected connection from'
-':53: no such host'
-'restartCount":"[^01]"'
-'dispatcher is stopped'
-'No installed keys could decrypt the message'
-'unsynchronized systime with swarm'
-'OVERLAP on Network'
-'no space left on device'
-'cannot allocate memory'
-'Some RethinkDB data on this server has been placed into swap memory'
+	'left gossip cluster'
+	"healthscore:[2-9] (connectivity issues)"
+	'with result "error:context canceled" took too long '
+	'unsynchronized systime with swarm'
+	'has prevented the request from succeeding (get secrets)'
+	'level.*error.* Cannot connect to the Docker daemon at tcp:'
+	'Error from leadership election follower'
+	'Cluster leadership lost'
+	"heartbeat to manager .* failed"
+	'dispatcher is stopped'
+	'level=error msg="periodic bulk sync failure for network '
+	": rejected connection from .* tcp "
+	"memberlist: Failed fallback ping: read tcp .* read: connection reset by peer"
+	'but other probes failed, network may be misconfigured'
+	' Some RethinkDB data on this server has been placed into swap '
+	'is in state down: heartbeat failure for node in'
+	'is in state down: Unhealthy UCP manager: ERROR: RethinkDB Health check timed out'
+	'is in state down: Awaiting healthy status in classic node inventory - current status: Unhealthy'
+	'etcd cluster is unavailable or misconfigured'
+	' martian source '
+	'Failed to create existing container'
+	'failed to allocate network IP for task '
+	'Failed to allocate address: Invalid address space'
+	'Failed to delegate: Failed to allocate address: No available addresses'
+	'"fatal task error" error="starting container failed: Address already in use"'
+	'deleteServiceInfoFromCluster NetworkDB DeleteEntry failed for '
+	'Failed to start certificate controller: error reading CA cert file'
+	'Failed to load config file'
+	'failed to re-resolve dtr-rethinkdb-'
+	'unable to query [dD][bB]: rethinkdb'
+	'unable to create event in database: rethinkdb: Cannot perform write:'
+	'unable to create job: unable to insert job into db: rethinkdb: Cannot perform write:'
+	'RethinkDB Health check timed out'
+	'failed to complete security handshake from'
+	'Err :connection error: desc = "transport: authentication handshake failed: read tcp '
+	"http: TLS handshake error from * tls: client didn't provide a certificate"
+	"tls: failed to verify client's certificate: x509: certificate has expired or is not yet valid"
+	'level=error .* x509: certificate signed by unknown authority'
+	': rejected connection from .* tls: .* certificate", ServerName '
+	': rejected connection from .* tls: .* certificate: x509: certificate has '
+	': rejected connection from .* "tls: .* does not match any of DNSNames '
+	'"OOMKilled":true'
+	'invoked oom-killer'
+	'[Cc]onnection refused '
+	'HTTP error: Unable to reach primary cluster manager '
+	'nfs: server  not responding, still trying'
+	':53: no such host'
+	'No installed keys could decrypt the message'
+	'[Nn]o space left on device'
+	'cannot allocate memory'
+	'error detaching from network .*: could not find network attachment for container .* to network '
+	'FieldPath:"spec.containers{calico-node}"}, Reason:"Unhealthy", Message:"Liveness probe failed:'
+	'FieldPath:"spec.containers{calico-node}"}, Reason:"Unhealthy", Message:"Readiness probe failed:'
+	"Unable to route request"
+	"Legacy license failure"
+	'"level":"fatal"'
+	'LOG_LEVEL=debug'
+	'OVERLAP on Network'
 )
 
 for PATTERN in "${PATTERNS[@]}"
 do
-  search_sd
+	search_sd
 done
 grep -r LOG_LEVEL=debug . ## to be sure that no containers of the cluster have been forgotten on a debug level
